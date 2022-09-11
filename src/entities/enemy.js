e4f6db6d1b63beb3fedac5f87435257ef726ba10
Christian Staudte <t4khosu@gmail.com>
2022-09-11 20:08:32 +0200

    Random color ghosts

--- src/entities/enemy.js
+++ src/entities/enemy.js
@@ -13,13 +13,17 @@ export class Enemy extends SpriteClass {
     z = 2
     time = 0
 
+    r = randInt(50, 255)
+    g = randInt(50, 255)
+    b = randInt(50, 255)
+
     constructor(level) {
         super({
             level: level,
             name: generateNameByDifficulty(level.difficulty),
             render: () => {
                 let ctx = this.context
-                ctx.fillStyle = `rgba(255, 0, 0, ${this.transparency()})`
+                ctx.fillStyle = `rgba(${this.r}, ${this.g}, ${this.b}, ${this.transparency()})`
                 ctx.strokeStyle = `rgba(0, 0, 0, ${this.transparency() * 2})`
                 ctx.beginPath()
                 ctx.arc(15, 15, 15, Math.PI, 2*Math.PI)
